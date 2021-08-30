cd29eecdcc83b595f7bfc5dd551351a4c328be54
TranQuangLuong <89313430+TranQuangLuong@users.noreply.github.com>
2021-08-30 16:28:31 +0700

    Add files via upload

--- text.dart
+++ text.dart
@@ -1,107 +1,84 @@
 import 'package:flutter/material.dart';
 import "main.dart";
-
 class App extends StatefulWidget {
-  
-  static final String title = 'TextFormField';
+
+ static final String title = 'TextFormField';
 
   @override
   _AppState createState() => _AppState();
-   
-
+  
 }
-
 class _AppState extends State<App> {
 final formKey = GlobalKey<FormState>();
-
 String username = '';
-
 String Signln = '';
-
 String Password ='';
-
 String LoginLn ='';
-
- bool value = false;
-
-  @override
-  Widget build( BuildContext context){
-    var body;
-    return Scaffold(
-      body: SafeArea(
-        key: formKey,
-        child: Container(                                   //Sign In To
+ bool value = false;  @override
+ Widget build( BuildContext context){
+   var body;
+  return Scaffold(
+    body: SafeArea(
+      key: formKey,
+       child: Container(                                   //Sign In To
              margin: EdgeInsets.all(15.0),
-          width: double.infinity,
-          height: double.infinity,
-            child: Column(
-        
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-
-              children : <Widget>[
-              Container(    
-                margin: EdgeInsets.all(10),                                  //Images
-                  child: Image( 
-                    image:
-                    AssetImage ('assets/one.jpg'),
-                    ),
-                ),
-                SizedBox ( height: 10 ,),
-               Container(
-                 margin: EdgeInsets.all(15), 
-             child:  Text(" Sign In ",
-                     
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                ),
-                
-                ),
+        width: double.infinity,
+        height: double.infinity,
+            child: Column(       
+            mainAxisAlignment: MainAxisAlignment.start,
+             crossAxisAlignment: CrossAxisAlignment.start,
+            children : <Widget>[
+              Container(                                           //Images
+                margin: EdgeInsets.all(10),                                  
+                 child: Image( 
+                  image:
+                   AssetImage ('assets/one.jpg'),
+                   ),
                ),
-                  SizedBox(height: 15,),
-                Column(
-                
-                 crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-      Container( 
+              SizedBox ( height: 10 ,),
+             Container(
+               margin: EdgeInsets.all(15), 
+             child:  Text(" Sign In ",                    
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                 fontSize: 25,
+                ),               
+              ),
+             ),
+                SizedBox(height: 15,),
+                Column(              
+                crossAxisAlignment: CrossAxisAlignment.start,
+                 children: <Widget>[
+     Container( 
         padding : EdgeInsets.symmetric(vertical: 3.0),
           margin: EdgeInsets.all(5.0), // cang le` 2 ben 
-             alignment: Alignment.centerLeft,
-             decoration: BoxDecoration(
+           alignment: Alignment.centerLeft,
+           decoration: BoxDecoration(
               color: Color(0xffEFEFEF),
-              borderRadius: BorderRadius.circular(20.0),// vo tron` 4 goc
-              
-            ),
-                
-            child:  TextFormField(                            //Username
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 15),
-                          border: InputBorder.none,
-                          labelText: 'Username',
-                          labelStyle: TextStyle(
-                            fontSize: 15,
+              borderRadius: BorderRadius.circular(20.0),// vo tron` 4 goc             
+            ),            
+           child:  TextFormField(                            //Username
+                       decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(left: 15),
+                         border: InputBorder.none,
+                        labelText: 'Username',
+                         labelStyle: TextStyle(
+                          fontSize: 15,
                             color: Colors.black26,
-                          ),
-                       hintText:username,
-                hintStyle: TextStyle(
-                  color: Colors.black87
-                )
-                          ),
-
                         ),
-                        
-                         
-            ),]
-            
-                       ),
-                  
-                        SizedBox(height: 10,),
-
+                     hintText:username,
+               hintStyle: TextStyle(
+                  color: Colors.black87,
+               )
+                        ),
+                        ),                                               
+            ),]           
+                       ),                  
+                       SizedBox(height: 10,),
                Column(
                 
                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
+                children: <Widget>[
       Container( 
         padding : EdgeInsets.symmetric(vertical: 3.0),
           margin: EdgeInsets.all(5.0), // cang le` 2 ben 
@@ -110,31 +87,28 @@ String LoginLn ='';
               color: Color(0xffEFEFEF),
               borderRadius: BorderRadius.circular(20.0),// vo tron` 4 goc
               
-            ),
-                
+           ),
+              
             child:  TextFormField(                            //Email
-                        decoration: InputDecoration(
+                      decoration: InputDecoration(
                           contentPadding: EdgeInsets.only(left: 15),
-                          border: InputBorder.none,
+                        border: InputBorder.none,
                           labelText: 'email',
-                          labelStyle: TextStyle(
-                            fontSize: 15,
+                         labelStyle: TextStyle(
+                          fontSize: 15,
                             color: Colors.black26,
-                          ),
+                        ),
                        hintText:Signln,
                 hintStyle: TextStyle(
                   color: Colors.black87
                 )
-                          ),
+                         ),
 
-                        ),
-                        
-                         
-            ),]
-            
-                       ),
-                  
-                           SizedBox(height: 10,),
+                      ),
+                                               
+            ),]           
+                       ),                  
+                         SizedBox(height: 10,),
                       Column(
                 
                  crossAxisAlignment: CrossAxisAlignment.start,
@@ -145,24 +119,21 @@ String LoginLn ='';
              alignment: Alignment.centerLeft,
              decoration: BoxDecoration(
               color: Color(0xffEFEFEF),
-              borderRadius: BorderRadius.circular(20.0),// vo tron` 4 goc
-              
+              borderRadius: BorderRadius.circular(20.0),// vo tron` 4 goc           
             ),
-                
             child:  TextFormField(                            //Password
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 15),
-                          
-                          border: InputBorder.none,
-                          labelText: 'Password',
-                          labelStyle: TextStyle(
-                            fontSize: 15,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 15),                       
+                      border: InputBorder.none,
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                          fontSize: 15,
                             color: Colors.black26,
-                          ),
-                       hintText:Password,
-                hintStyle: TextStyle(
-                  color: Colors.black87
-                )
+                      ),
+                          hintText:Password,
+        hintStyle: TextStyle(
+                  color: Colors.black87,
+                ),
                           ),
 
                         ),
@@ -172,9 +143,9 @@ String LoginLn ='';
             
                        ),
                   
-                                        SizedBox(height: 10,),
-                                       
-                                           new Row(
+                                       SizedBox(height: 10,),
+
+                                           new Row(              //Stay Logged In //Forgotten Password?
                
                                                         
                                     children: <Widget>[                                             
@@ -188,7 +159,7 @@ String LoginLn ='';
                                                   decoration: new BoxDecoration(
                                                     border: Border.all(
                                                       width: 1,
-                                                  ),
+                                                ),
                                                     borderRadius: new BorderRadius.circular(5),
                                                   ),
                                                   child: Theme(
@@ -197,7 +168,7 @@ String LoginLn ='';
                                                   ),
                                                     child: Checkbox(
                                                       
-                                                      value: value,
+                                                     value: value,
                                                       onChanged: (state) =>
                                                           setState(() => value = !value),
                                                       activeColor: Color(0xff5ABD8C),
@@ -227,9 +198,9 @@ String LoginLn ='';
                                                     style: TextStyle(
                                                        color: Color(0xff707070), 
                                                          fontWeight: FontWeight.normal                        
-                                                  ),
-                                                       ),
-                                                          ),
+                                                ),
+                                                      ),
+                                                        ),
                                                            ),                                           
                                                   ],                                                                                    
                                                  ),                                       
@@ -240,18 +211,18 @@ String LoginLn ='';
                           decoration: BoxDecoration(                       
                             color: Color(0xff5ABD8C),
                               borderRadius: BorderRadius.circular(20),
-                                 boxShadow: [
+                               boxShadow: [
                                   BoxShadow(
                                     color: Color(0xff5ABD8C),
                                       blurRadius: 15,
                                         offset: Offset(0,10)
-                                  )
+                                )
                                ],                             
-                             ),
+                            ),
                               child: Center(      
-                                  child: InkWell(
-                                  onTap: () {
-                                      print(" Sign Ln");
+                                child: InkWell(
+                                onTap: () {
+                                    print(" Sign Ln");
                                          },                                  
                                 child: Text(
                                   " Sign Ln ",
@@ -260,23 +231,17 @@ String LoginLn ='';
                                       fontSize: 20,
                                       fontWeight: FontWeight.normal                                  
                                       ),                                    
-                                    ),
                                   ),
-                                 ),
+                                ),
+                                ),
                                 )                           
-                              ],
-                             ),     
+                             ],
+                           ),     
                             ),  
-                ),
-              );
-    
-   
-    
-      
-    
-
-    }
-
+              ),
+              );   
+     
+   }
   
 }
 
